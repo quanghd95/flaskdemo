ede1060732c57f4044e472dc01ccea579117cd52
kyanh2904 <nguyenvukyanh2904@gmail.com>
2023-03-13 19:33:55 +0700

    flaskupdated

--- kyanh/flask.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask 
-app = Flask(__name__)
-
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
-
-def index():
-    return render_template('{user_name}.html')
