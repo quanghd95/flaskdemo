93c9e3460b8b67b52dae59a8e479eaca3ca400bd
Haquang <quanghd@gmail.com>
2023-03-20 18:53:36 +0700

    template example

--- haquang/haquang.py
+++ haquang/haquang.py
@@ -7,3 +7,15 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('haquang.html')
+
+@app.route('/list_user/')
+def list_user():
+    users = [
+        {"name": "quang", "href": "https://github.com/quanghd95/flaskdemo"},
+        {"name": "ha", "href": "https://github.com/quanghd95/Python4KId"},
+        {"name": "xxxx", "href": "https://github.com/quanghd95/Python4KId"}
+    ]
+
+    a = "Hello world"
+    return render_template('list_user.html', users = users, a = a)
+
