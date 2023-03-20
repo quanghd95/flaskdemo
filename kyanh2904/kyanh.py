e7ec2025a35dd2787744a103f771c2b10d253457
kyanh2904 <nguyenvukyanh2904@gmail.com>
2023-03-20 18:39:31 +0700

    kyanhflask/home

--- kyanh2904/kyanh.py
+++ kyanh2904/kyanh.py
@@ -1,7 +1,11 @@
-from flask import Flask 
-from flask import render_template
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return render_template('kyanh.html')
+@app.route("/home", methods=["GET", "POST"])
+def home():
+    if request.method == "POST":
+        name = request.form.get("name")
+        return render_template("kyanh.html", name=name)
+    else:
+        return render_template("kyanh.html")
